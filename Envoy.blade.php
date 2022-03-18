e56c3f6c12d0fe6367b97cb0e728d7c5b09a1bd3
geozhur <georgyzhur@gmail.com>
2022-03-18 14:05:25 +0100

    remove jetstream

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,12 +5,14 @@
 @endsetup
 @task('deploy', ['on' => 'main'])
     cd /data/geozhur/www/
+    php artisan down
     git checkout master
+    git reset --hard HEAD;git clean -df
     git pull
     composer install --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
+    composer dumpautoload
     php artisan migrate --force
-    php artisan cache:clear
-    php artisan config:cache
+    php artisan optimize:clear
     npm install
     npm run prod
     php artisan up
